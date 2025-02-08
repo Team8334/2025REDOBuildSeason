cc5b560487f8e7d19f09b486b2460a46bcb9e53b
FRC8334Students <122326994+echo2334@users.noreply.github.com>
2025-02-08 10:44:22 -0700

    started on the logic for scoring control. the idea is to have it be as easy as Aeode was to operate

--- 2025REDOBuildSeason/src/main/java/frc/robot/Subsystem/ScoringControl.java
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Subsystem/ScoringControl.java
@@ -3,32 +3,52 @@ package frc.robot.Subsystem;
 import frc.robot.Data.PortMap;
 
 import frc.robot.Devices.NEOSparkMaxMotor;
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.ConfigurationFailedException;
 
-public class EndEffector implements Subsystem {
+public class ScoringControl implements Subsystem {
 
     //case statement has nothing to make this class work. delete this comment when you have added the necessary changes.
     //much testing is need for this branch. delete this comment when you have tested it.
 
-    private static EndEffector instance = null;
+    private static ScoringControl instance = null;
 
     private NEOSparkMaxMotor effectorMotorUno = new NEOSparkMaxMotor(PortMap.EFFECTOR_MOTOR_1);
     private NEOSparkMaxMotor effectorMotorDos = new NEOSparkMaxMotor(PortMap.EFFECTOR_MOTOR_2);
 
+    private LaserCan lc = new LaserCan(PortMap.LASER_CAN);
+
     private double effectorUno;
     private double effectorDos;
+
 
     public String state = "passive";
 
 
-    public static EndEffector getInstance() {
+    public static ScoringControl getInstance() {
         if (instance == null) {
-            instance = new EndEffector();
+            instance = new ScoringControl();
         }
         return instance;
     }
 
-    public EndEffector(){
+    public ScoringControl(){
         SubsystemManager.registerSubsystem(this);
+    }
+
+    public void EffectorRun(){
+        effectorMotorUno.set(effectorUno);
+        effectorMotorDos.set(effectorDos);
+    }
+
+    public void laserConfig(){
+        try {
+            lc.setRangingMode(LaserCan.RangingMode.SHORT);
+            lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+            lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+          } catch (ConfigurationFailedException e) {
+            System.out.println("Configuration failed! " + e);
+          }
     }
 
     public void Passive(){
@@ -55,22 +75,32 @@ public class EndEffector implements Subsystem {
         switch (state)
         {
             case "passive":
+                    effectorUno = 0.0;
+                    effectorDos = 0.0;
 
                 break;
 
             case "ramp detects coral":
+                    effectorUno = 0.0;
+                    effectorDos = 0.0;
                 
                 break;
 
             case "transfering coral to effector":
+                    effectorUno = 0.5;
+                    effectorDos = 0.5;
 
                 break;
 
             case "coral tripped sensor":
+                    effectorUno = 0.0;
+                    effectorDos = 0.0;
 
                 break;
             
             case "ejecting coral":
+                    effectorUno = -0.5;
+                    effectorDos = -0.5;
 
                 break;
 
