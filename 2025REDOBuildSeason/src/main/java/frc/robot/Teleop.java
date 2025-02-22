dd72a7ffc5b877ce2842253d056a80ecc48cb791
FRC8334Students <frc8334student@gmail.com>
2025-02-22 16:29:34 -0700

    Working on alignment
    This code should let the driver hit the left bumper to inch left when in front of the reef apriltags and hit the right bumper to inch right. They can also hit a to try to align with the reef using the limelight while in teleop. Ignore this class's version of scoring logic.

--- 2025REDOBuildSeason/src/main/java/frc/robot/Teleop.java
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Teleop.java
@@ -3,16 +3,24 @@ package frc.robot;
 import frc.robot.Devices.Controller;
 import frc.robot.Data.PortMap;
 import frc.robot.Subsystem.Mecanum;
-
+import frc.robot.Subsystem.Targeting;
+import frc.robot.Subsystem.FrontLimelight;
+import frc.robot.Subsystem.Alignment;
 
 public class Teleop {
     
-    Controller driverController;
+        Controller driverController;
     
         Mecanum mecanum;
+        Alignment alignment;
         private double controllerLeftX;
         private double controllerLeftY;
         private double controllerRightX;
+
+        private boolean aligning = false;
+        private boolean aButtonPressed;
+        private boolean rightBumperPressed;
+        private boolean leftBumperPressed;
     
         public Teleop() {
             driverController = new Controller(PortMap.DRIVER_CONTROLLER);
@@ -21,6 +29,11 @@ public class Teleop {
             }
     
             mecanum = Mecanum.getInstance();
+            alignment = Alignment.getInstance();
+
+            aButtonPressed = driverController.getAButtonPressed();
+            rightBumperPressed = driverController.getRightBumperButtonPressed();
+            leftBumperPressed = driverController.getLeftBumperButtonPressed();
         }
     
         public void teleopPeriodic() {
@@ -28,31 +41,49 @@ public class Teleop {
         }
     
         public void driveBaseControl() {
-           controllerLeftY = driverController.getLeftY();
-           controllerLeftX = driverController.getLeftX();
-           controllerRightX = driverController.getRightX();
-           double forward;
-           double strafe;
-           double rotation;
-           if(Math.abs(controllerLeftY) >= 0.5){
-            forward = (controllerLeftY*0.5);
-           }
-           else{
-            forward = 0;
-           }
-           if(Math.abs(controllerLeftX) >= 0.5){
-            strafe = (controllerLeftX*0.5);
-           }
-           else{
-            strafe = 0;
-           }
-           if(Math.abs(controllerRightX) >= 0.2){
-            rotation = (controllerRightX*0.35);
-           }
-           else{
-            rotation = 0;
-           }
-           mecanum.drive(forward, strafe, rotation);
+            controllerLeftY = driverController.getLeftY();
+            controllerLeftX = driverController.getLeftX();
+            controllerRightX = driverController.getRightX();
+            double forward;
+            double strafe;
+            double rotation;
+            
+            if(aButtonPressed){
+                aligning = true;
+                alignment.alignReef();
+            }
+            if(rightBumperPressed){
+                aligning = true;
+                alignment.alignRight();
+            }
+            if(leftBumperPressed){
+                aligning = true;
+                alignment.alignLeft();
+            }
+            if(!aButtonPressed && !rightBumperPressed && !leftBumperPressed){
+                aligning = false;
+            }
+            if(!aligning){
+                if(Math.abs(controllerLeftY) >= 0.5){
+                    forward = (controllerLeftY*0.5);
+                }
+                else{
+                    forward = 0;
+                }
+                if(Math.abs(controllerLeftX) >= 0.5){
+                    strafe = (controllerLeftX*0.5);
+                }
+                else{
+                    strafe = 0;
+                }
+                if(Math.abs(controllerRightX) >= 0.2){
+                    rotation = (controllerRightX*0.35);
+                }
+                else{
+                    rotation = 0;
+                }
+                mecanum.drive(forward, strafe, rotation);
+            }
           
         }
     
