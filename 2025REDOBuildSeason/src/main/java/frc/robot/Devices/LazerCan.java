e6debc85a5f49cdacec6539b0e36a887d7bbc31e
BismuthTime <122326994+echo2334@users.noreply.github.com>
2025-03-06 18:09:36 -0700

    elise did not commit her code. for shame, for shame.

---
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Devices/LazerCan.java
@@ -0,0 +1,38 @@
+package frc.robot.Devices;
+
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.interfaces.LaserCanInterface.Measurement;
+
+import frc.robot.Data.PortMap;
+
+public class LazerCan {
+     private LaserCan lc; 
+     public int laserDetectedDistance;
+
+     public LazerCan(int LazerCanID){
+            lc = new LaserCan(LazerCanID);
+            laserConfig();
+     }
+     public void laserConfig(){
+        try {
+            lc.setRangingMode(LaserCan.RangingMode.SHORT);
+            lc.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+            lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+          } 
+        catch (ConfigurationFailedException e) {
+            System.out.println("Configuration failed! " + e);
+        }
+    }
+
+    public double laserDistance(){
+        LaserCan.Measurement measurement = lc.getMeasurement();
+        if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT){
+            measurement.distance_mm = laserDetectedDistance;
+            System.out.println("The target is " + measurement.distance_mm + "mm away!");
+        } else{
+            laserDetectedDistance = 999999;
+        }
+        return laserDetectedDistance;
+    }
+}
