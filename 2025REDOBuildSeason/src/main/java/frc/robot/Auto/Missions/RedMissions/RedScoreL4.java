3cbdae8e8c21ac7120a3952d08c65588eb9f8295
FRC8334Students <122326994+echo2334@users.noreply.github.com>
2025-03-17 19:39:21 -0600

    started dead reckoning autos

--- 2025REDOBuildSeason/src/main/java/frc/robot/Auto/Missions/RedMissions/RedScoreL4.java
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Auto/Missions/RedMissions/RedScoreL4.java
@@ -24,8 +24,8 @@ public class RedScoreL4 extends MissionBase{
     @Override
     protected void routine() throws AutoMissionEndedException {
        
-        runAction (new FrontLockOnAction("Reef",true,2));//pls change number
-        runAction (new FrontAlignAction("Reef","left",10)); //pls change number
+        //runAction (new FrontLockOnAction("Reef",true,2));//pls change number
+        //runAction (new FrontAlignAction("Reef","left",10)); //pls change number
         runAction (new MoveElevatorAction(0.5, States.SCOREL2));
        // runAction (new EffectorAction(0.2, 0.2));
         runAction (new DriveForTimeAction(0.1, 0.2));
