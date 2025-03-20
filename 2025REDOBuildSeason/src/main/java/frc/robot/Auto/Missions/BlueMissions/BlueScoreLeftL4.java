f9010945b8819f9380e1adf92b89fb156ee7bfb4
BismuthTime <122326994+echo2334@users.noreply.github.com>
2025-03-20 15:37:56 -0600

    Changed some values
    still needs to be tested.

--- 2025REDOBuildSeason/src/main/java/frc/robot/Auto/Missions/BlueMissions/BlueScoreLeftL4.java
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Auto/Missions/BlueMissions/BlueScoreLeftL4.java
@@ -20,7 +20,7 @@ public class BlueScoreLeftL4 extends MissionBase{
        
         runAction (new DriveForTimeAction(-0.3, 2.1));
         runAction (new TurnDegreesAction(-22, 1));
-        runAction (new DriveForTimeAction(-0.3, 0.5));
+        runAction (new DriveForTimeAction(-0.2, 0.5));
         runAction (new MoveElevatorAction(1.5, States.SCOREL4));
         runAction (new EffectorAction(0.2, States.SCORING));
         runAction (new DriveForTimeAction(0.1, 0.3));
