c05d50bae612d0a48caba4577de209e29cfcb63e
FRC8334Students <frc8334student@gmail.com>
2025-03-21 13:40:51 -0600

    regetting limelight stuff

---
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Auto/Actions/LockOnAction.java
@@ -0,0 +1,103 @@
+package frc.robot.Auto.Actions;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Subsystem.Mecanum;
+import frc.robot.Data.Debug;
+import frc.robot.Subsystem.Alignment;
+import frc.robot.Subsystem.FrontLimelight;
+import frc.robot.Subsystem.Mecanum;
+
+public class LockOnAction implements Actions
+{
+    private Mecanum mecanum;
+    private Alignment alignment;
+
+    private FrontLimelight frontLimelight;
+    private String target;
+    private boolean driveTo;
+
+    Timer timer;
+    double seconds;
+    double neededArea;
+
+    /**
+     * Run code once when the action is started, for setup
+     */
+    public LockOnAction(String target, boolean driveTo, double seconds)
+    {
+        this.driveTo = driveTo;
+        this.target = target;
+        alignment = Alignment.getInstance();
+        frontLimelight = FrontLimelight.getInstance();
+        mecanum = Mecanum.getInstance();
+        frontLimelight.setPipeline(0);
+
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void start()
+    {
+        timer = new Timer();
+        timer.start();
+
+        if(Debug.debug){
+        SmartDashboard.putString("Current Action", "FrontLockOnAction Started");
+        }
+    }
+
+    /**
+     * Called by runAction in AutoModeBase iteratively until isFinished returns
+     * true. Iterative logic lives in this
+     * method
+     */
+    @Override
+    public void update()
+    {
+        if ((frontLimelight.findTagName() != "Unknown") && driveTo && frontLimelight.getPipeline() == 0)
+        {
+            alignment.driveTo(target);
+        }
+        if((frontLimelight.findTagName() != "Unknown") && !driveTo && frontLimelight.getPipeline() == 0)
+        {
+            alignment.alignXAngle(target);
+        }
+        else
+        {
+            mecanum.driveWithSpeed(0, 0, 0);
+        }
+    }
+
+    /**
+     * Returns whether or not the code has finished executaprilTagLockOnementing
+     * this interface, this method is used by
+     * the runAction method every cycle to know when to stop running the action
+     *
+     * @return boolean
+     */
+    @Override
+    public boolean isFinished()
+    {
+        if (timer.get() >= seconds) 
+        {
+            return true;
+        } 
+        else 
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Run code once when the action finishes, usually for clean up
+     */
+    @Override
+    public void done()
+    {
+        mecanum.driveWithSpeed(0, 0, 0);
+    }
+
+}
+
