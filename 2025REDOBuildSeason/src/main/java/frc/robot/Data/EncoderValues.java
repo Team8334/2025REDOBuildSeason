f5238e9c015b98ad3f9560bded7a31b750b1a66a
FRC8334Students <122326994+echo2334@users.noreply.github.com>
2025-03-22 15:05:54 -0600

    final update for encoder values. great competition everyone, i'm proud.

--- 2025REDOBuildSeason/src/main/java/frc/robot/Data/EncoderValues.java
+++ 2025REDOBuildSeason/src/main/java/frc/robot/Data/EncoderValues.java
@@ -7,11 +7,11 @@ public class EncoderValues {
     //numbers you see right now. during calibration, double check these values work, and if not, follow
     //those steps to change them.
 
-    public static final double ELEVATOR_RAMP = -0.32;
+    public static final double ELEVATOR_RAMP = -0.41;
     public static final double ELEVATOR_L1 = 0.04; // 46cm
-    public static final double ELEVATOR_L2 = 0.61; //81cm
-    public static final double ELEVATOR_L3 = 2.0; //121 cm
-    public static final double ELEVATOR_L4 = 4.22; // 183cm
+    public static final double ELEVATOR_L2 = 0.62; //81cm
+    public static final double ELEVATOR_L3 = 2.01; //121 cm
+    public static final double ELEVATOR_L4 = 4.24; // 183cm
     public static final double ELEVATOR_BARGE = 4.36;
     public static final double ELEVATOR_LOWER_ALGAE = -0.40;
     public static final double ELEVATOR_UPPER_ALGAE = 1.11;
